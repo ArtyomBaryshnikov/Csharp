85ca0e16c4ccde240d6ff7ff61e664007c578589
E-noTi-kgit config --global user.name E-noTi-kgit config --global user.name E-noTi-k <labovichaleks@gmail.com>
2024-11-18 16:27:28 +0300

    Сделали сумму чисел

--- Задание 2/Задание 2/Program.cs
+++ Задание 2/Задание 2/Program.cs
@@ -4,3 +4,5 @@ Console.Write("Введите 2 число");
 int b = int.Parse(Console.ReadLine());
 int p = a * b;
 Console.WriteLine($"Произведение чисел равно = {p}");
+int s = a + b;
+Console.WriteLine($"Сумма чисел равно = {s}");
